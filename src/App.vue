0468792c1600ec08c964a25817988830b2228ff6
wjjwkwindy <wjjwkwindy@gmail.com>
2025-05-14 10:40:19 +0800

    chore: 添加@vercel/speed-insights

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
   <router-view></router-view>
+  <SpeedInsights />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
